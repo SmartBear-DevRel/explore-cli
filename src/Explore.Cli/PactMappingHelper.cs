7990e4b2f2b4dad9cb09d3d33264948ba1739b66
Yousaf Nabi <you@saf.dev>
2024-05-22 14:24:03 +0100

    feat: create hasPactVersion / getPactVersion funcs

--- src/Explore.Cli/PactMappingHelper.cs
+++ src/Explore.Cli/PactMappingHelper.cs
@@ -8,32 +8,7 @@ using Newtonsoft.Json.Schema;
 public static class PactMappingHelper
 {
 
-    public static bool isPact(string json)
-    {
-        var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
-
-        if (jsonObject != null)
-        {
-            var schemaJson = @"{
-                'description': 'A person',
-                'type': 'object',
-                'properties':
-                {
-                    'name': {'type':'string'},
-                    'hobbies': {
-                    'type': 'array',
-                    'items': {'type':'string'}
-                    }
-                }
-                }";
-            var  schema = JsonSchema.Parse(schemaJson);
-                
-        }
-
-        return false;
-    }
-
-    public static bool isPactV4(string json)
+    public static bool hasPactVersion(string json)
     {
         var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
 
@@ -46,63 +21,42 @@ public static class PactMappingHelper
                 if (pactSpecification != null && pactSpecification["version"] != null && pactSpecification["version"].ToString() != null)
                 {
                     var version = pactSpecification["version"].ToString();
-                    if (string.Equals(version, "4.0.0", StringComparison.OrdinalIgnoreCase))
+                    var validVersions = new List<string> { "1.0.0", "2.0.0", "3.0.0", "4.0.0" };
+                    if (version != null && validVersions.Contains(version))
                     {
                         return true;
                     }
                 }
-
             }
         }
 
         return false;
     }
-    public static bool isPactV3(string json)
-    {
-        var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
 
-        if (jsonObject != null && jsonObject.ContainsKey("metadata"))
+        public static string getPactVersion(string json)
         {
-            var metadata = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonObject["metadata"].ToString() ?? string.Empty);
-            if (metadata != null && metadata.ContainsKey("pactSpecification"))
+            var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+
+            if (jsonObject != null && jsonObject.ContainsKey("metadata"))
             {
-                var pactSpecification = JsonSerializer.Deserialize<Dictionary<string, object>>(metadata["pactSpecification"].ToString() ?? string.Empty);
-                if (pactSpecification != null && pactSpecification["version"] != null && pactSpecification["version"].ToString() != null)
+                var metadata = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonObject["metadata"].ToString() ?? string.Empty);
+                if (metadata != null && metadata.ContainsKey("pactSpecification"))
                 {
-                    var version = pactSpecification["version"].ToString();
-                    if (string.Equals(version, "3.0.0", StringComparison.OrdinalIgnoreCase))
+                    var pactSpecification = JsonSerializer.Deserialize<Dictionary<string, object>>(metadata["pactSpecification"].ToString() ?? string.Empty);
+                    if (pactSpecification != null && pactSpecification["version"] != null && pactSpecification["version"].ToString() != null)
                     {
-                        return true;
+                        var version = pactSpecification["version"].ToString();
+                        var validVersions = new List<string> { "1.0.0", "2.0.0", "3.0.0", "4.0.0" };
+                        if (version != null && validVersions.Contains(version))
+                        {
+                            return $"pact-v{version}";
+                        }
                     }
                 }
-
             }
+
+            return string.Empty;
         }
 
-        return false;
-    }
-    public static bool isPactV2(string json)
-    {
-        var jsonObject = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
-
-        if (jsonObject != null && jsonObject.ContainsKey("metadata"))
-        {
-            var metadata = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonObject["metadata"].ToString() ?? string.Empty);
-            if (metadata != null && metadata.ContainsKey("pactSpecification"))
-            {
-                var pactSpecification = JsonSerializer.Deserialize<Dictionary<string, object>>(metadata["pactSpecification"].ToString() ?? string.Empty);
-                if (pactSpecification != null && pactSpecification["version"] != null && pactSpecification["version"].ToString() != null)
-                {
-                    var version = pactSpecification["version"].ToString();
-                    if (string.Equals(version, "2.0.0", StringComparison.OrdinalIgnoreCase))
-                    {
-                        return true;
-                    }
-                }
-
-            }
-        }
-
-        return false;
-    }
+ 
 }
